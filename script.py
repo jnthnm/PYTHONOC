6b75d332b16845e89bcfa2afa97d898a3f12f7dd
Jonathan M <nknlol75@gmail.com>
2020-06-27 00:36:39 +0200

    Etape 3 Auth aws configure et Etape 4 (Upload AWS) OK

--- script.py
+++ script.py
@@ -1,8 +1,6 @@
 
-# Le but est de crée un script de sauvegarde d'un fichier
-# en particulier, et l'envoyer sur mon srv S3 AWS
- 
-# import datetime
+# Le but est de crée un script de sauvegarde de deux fichiers
+#  et l'envoyer sur mon srv S3 AWS dans le bucket pythonscriptoc
 
 # 1 - Lister tous les fichiers qui sont dans D:\Jonathan\Documents
 
@@ -14,7 +12,6 @@ print(doclist)
 
 # 2 - En faire un ZIP
 import zipfile
-
 # Je décide de crée mon fichier zip dans D:\Jonathan\Documents\ 'w : writemod'
 my_zip = zipfile.ZipFile('D:\Jonathan\Documents\montest.zip', 'w')
 
@@ -26,11 +23,15 @@ my_zip.close()
 
 # 3 - Utiliser CLI AWS pour s'authentifier (https://docs.aws.amazon.com/fr_fr/cli/latest/userguide/cli-chap-configure.html#cli-quick-configuration)
 
+# AWS CONFIGURE
+# On y rentre notre AWS Access Key ID & notre AWS Secret Access Key
+print('Auth AWS OK')
 
 # 4 - Push mon script sur mon serv S3 AWS (https://boto3.amazonaws.com/v1/documentation/api/latest/index.html) (https://docs.aws.amazon.com/AmazonS3/latest/user-guide/upload-objects.html)
 
+# Module boto3 
 import boto3
-s3 = boto3.resource('s3')
-s3.Bucket('pythonscriptoc').upload_file('D:\Jonathan\Documents\montest.zip')
 
-# 5 - L'integrer au planificateur de taches
+s3 = boto3.resource('s3')
+s3.meta.client.upload_file('D:\Jonathan\Documents\montest.zip', 'pythonscriptoc', 'montest.zip')
+print ('Fichier ZIP UPLOADER dans le bucket pythonscriptoc')
