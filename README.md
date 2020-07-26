b4ad379ead9f5a970a8636a2cb98920204e41002
Jonathan M <nknlol75@gmail.com>
2020-07-26 17:08:14 +0200

    Corrections

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ de supprimé également des fichiers avec une date défini
 Pre requis pour que le script fonctionne :
 
 - Python 3.7.8
+Indiquer pip python
 - CLI AWS
 - Github
 
@@ -17,5 +18,12 @@ On définit nos paramètres dans le fichier de configuration nommé config.json
 - "chemin": "Le Chemin des fichiers à sauvegarder",
 - "bucketnom": "Nom Du Bucket qui sera crée sur notre serveur S3 AWS",
 - "joursdesave": Le nombre de jours de sauvegardes avant la suppréssion du fichier
+
+
+
+
+
+
+
 
 
--- script.py
+++ script.py
@@ -19,6 +19,22 @@ with open('./config.json', 'r') as fichier:
 datetoday = datetime.date.today()
 print("La date d'aujourd'hui est : " + " " + str(datetoday))
 
+# Permet de verifié si le bucket est déja existant 
+bucketexist = False
+s3 = boto3.client('s3')
+response = s3.list_buckets()
+print('Existing buckets:')
+for bucket in response['Buckets']:
+    print(f'  {bucket["Name"]}')
+    if bucket["Name"] == parametres['bucketnom']:
+        bucketexist = True
+
+if bucketexist: 
+    print('Le bucket existe') 
+else:
+    print('Le bucket n\'existe pas')         
+    create_bucket(parametres['bucketnom'],"us-west-2")
+
 def create_bucket(bucket_name, region=None):
     # Création du bucket
     try:
@@ -34,7 +50,6 @@ def create_bucket(bucket_name, region=None):
         logging.error(e)
         return False
     return True
-create_bucket(parametres['bucketnom'],"us-west-2")
 
 # Je liste mes dossiers dans le chemin choisi
 doclist = os.listdir(parametres["chemin"])
@@ -61,8 +76,7 @@ for file in doclist:
             
 my_zip.close()
 
-# S'authentifié avec le CLI D'AWS
-# Push mon script sur mon serv S3 AWS (https://boto3.amazonaws.com/v1/documentation/api/latest/index.html) (https://docs.aws.amazon.com/AmazonS3/latest/user-guide/upload-objects.html)
+# Push mon script sur mon serv S3 AWS 
 
 def upload_to_aws(local_file, bucket, s3_file):
     s3 = boto3.client('s3')
@@ -76,38 +90,12 @@ def upload_to_aws(local_file, bucket, s3_file):
 
 uploaded = upload_to_aws('creationzip', parametres['bucketnom'], nomdoc)
 
-# Proceder au delete du fichier zip
-try:
-    os.remove(creationzip)
-except OSError as e:
-    print(e)
-else:
-    print("Le fichier zip nommé" + " " + nom_todelete + " " + "est supprimé")
-
-# Permet de verifié si le bucket est déja existant 
-bucketexist = False
-s3 = boto3.client('s3')
-response = s3.list_buckets()
-print('Existing buckets:')
-for bucket in response['Buckets']:
-    print(f'  {bucket["Name"]}')
-    if bucket["Name"] == parametres['bucketnom']:
-        bucketexist = True
-
-if bucketexist: 
-    print('Le bucket existe') 
-else:
-    print('Le bucket n\'existe pas')       
-
-print(nomdoc)
-
 # Delete du fichier défini dans le fichier de config.json
 s3 = boto3.resource("s3")
 obj = s3.Object(parametres['bucketnom'], nom_todelete)
 obj.delete()
 print("Le fichier zip nommé" + " " + nom_todelete + " " + "est supprimé du cloud S3 AWS du bucket nommé" + " " + parametres['bucketnom'])
 
-# Utiliser planificateur de tâches windows pour exec le script automatiquement
-# Choisir license libre
+
 
 
